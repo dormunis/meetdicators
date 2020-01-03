7a4438e74916c0e0d501dc053e3d1ae5691514a7
Dor Munis <dor.munis@planckresolution.com>
2020-01-03 16:01:32 +0200

    added carousel functionality to next meetings

--- lib/google/calendar.dart
+++ lib/google/calendar.dart
@@ -2,14 +2,9 @@ import 'dart:convert';
 
 import 'package:googleapis/calendar/v3.dart';
 import 'package:googleapis_auth/auth_io.dart';
-import 'dart:io';
+import 'package:flutter/services.dart';
 
 final int _maxEventResults = 5;
-
-final _calendarIds = json.decode(new File('calendar-ids.json').readAsStringSync());
-
-final _credentials = new ServiceAccountCredentials.fromJson(
-    new File('.service-account.json').readAsStringSync());
 
 const _SCOPES = const [
   CalendarApi.CalendarEventsReadonlyScope,
@@ -19,33 +14,64 @@ const _SCOPES = const [
   CalendarApi.CalendarSettingsReadonlyScope,
 ];
 
-void main() {
-  clientViaServiceAccount(_credentials, _SCOPES).then((httpClient) {
-    var calendar = new CalendarApi(httpClient);
-    final DateTime now = DateTime.now().toUtc();
-    final DateTime tomorrow =
-        DateTime(now.year, now.month, now.day + 1).toUtc();
-    calendar.events
-        .list(_calendarIds['einstein'],
-            maxResults: _maxEventResults,
-            timeZone: "UTC",
-            singleEvents: true,
-            orderBy: "startTime",
-            timeMin: now,
+Future<Object> _upcomingEvents(httpClient, room) async {
+  var calendar = new CalendarApi(httpClient);
+  final DateTime now = DateTime.now().toUtc();
+  final DateTime tomorrow =
+  DateTime(now.year, now.month, now.day + 1).toUtc();
+  return calendar.events.list(
+    room,
+    maxResults: _maxEventResults,
+    timeZone: "UTC",
+    singleEvents: true,
+    orderBy: "startTime",
+    timeMin: now,
 //            timeMax: tomorrow
-    )
-        .then((events) {
-      for (var event in events.items) {
-        print(event.summary);
-        print(
-            '${event.start.dateTime.toLocal().toString()} - ${event.end.dateTime.toLocal().toString()}');
-        print('attendees:');
-        for (var attendee in event.attendees) {
-          if (!attendee.email.endsWith('resource.calendar.google.com') && !attendee.responseStatus.contains('declined')) {
-            print('\t${attendee.email}');
-          }
-        }
+  );
+}
+
+Future<List> _parseEvents(events) async {
+  List parsedEvents = List();
+  for (var event in events.items) {
+    List attendees = List();
+    for (var attendee in event.attendees) {
+      if (!attendee.email.endsWith('resource.calendar.google.com') &&
+          !attendee.responseStatus.contains('declined')) {
+        attendees.add(attendee.email);
       }
+    }
+    parsedEvents.add({
+      'title': event.summary,
+      'start': event.start.dateTime.toLocal(),
+      'end': event.end.dateTime.toLocal(),
+      'attendees': attendees,
     });
-  });
+  }
+  return parsedEvents;
+}
+
+Future<String> _readAsset(String path) async {
+  return rootBundle.loadString(path);
+}
+
+Future<List> getCalendarEvents(room) async {
+  final _calendarIds = json.decode(await _readAsset('assets/google/calendar-ids.json'));
+  final _credentialsContents = json.decode(await _readAsset('assets/google/.service-account.json'));
+  final _credentials = new ServiceAccountCredentials.fromJson(_credentialsContents);
+  var httpClient = await clientViaServiceAccount(_credentials, _SCOPES);
+  var rawEvents = await _upcomingEvents(httpClient, _calendarIds[room]);
+  return await _parseEvents(rawEvents);
+}
+
+Future getCurrentMeeting(room) async {
+
+}
+
+Future<void> postEvent(String room, DateTime endTime) async {
+}
+
+
+Future<void> main() async {
+  List events = await getCalendarEvents('einstein');
+  print(events);
 }
--- lib/viewcomponents/currentmeeting.dart
+++ lib/viewcomponents/currentmeeting.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:occupied_room/google/calendar.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class CurrentMeeting extends StatefulWidget {
   @override
@@ -6,42 +9,96 @@ class CurrentMeeting extends StatefulWidget {
 }
 
 class _CurrentMeetingState extends State<CurrentMeeting> {
+  List _events;
+  static String _dateTimeFormat = "HH:mm";
+
+  Future<void> populateEvents() async {
+    _events = (await getCalendarEvents('einstein')).sublist(0);
+  }
+
+  Container generateEventView(event) {
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+              child: Column(children: <Widget>[
+            Text(
+              event['title'],
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.white, fontSize: 18),
+            )
+          ])),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        DateFormat(_dateTimeFormat).format(event['start']),
+                        style: TextStyle(color: Colors.white, fontSize: 24),
+                      ),
+                      Text(
+                        "-",
+                        style: TextStyle(color: Colors.white, fontSize: 24),
+                      ),
+                      Text(
+                        DateFormat(_dateTimeFormat).format(event['end']),
+                        style: TextStyle(color: Colors.white, fontSize: 24),
+                      )
+                    ])
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: <Widget>[
-        Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-          Text(
-            "Next meetings:",
-            style: TextStyle(
-                color: const Color(0xff3f515e),
-                fontWeight: FontWeight.bold,
-                fontSize: 42),
-          ),
-        ]),
-        Container(
-          color: const Color(0xff3f515e),
-          width: MediaQuery.of(context).size.width * 0.7,
-          child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  "Kaka bepita",
-                  style: TextStyle(color: Colors.white, fontSize: 24),
-                ),
-                Text(
-                  "\t\t-\t\t",
-                  style: TextStyle(color: Colors.white, fontSize: 24),
-                ),
-                Text(
-                  "10:00 - 11:30",
-                  style: TextStyle(color: Colors.white, fontSize: 24),
-                )
-              ]),
-        )
-      ],
+    return FutureBuilder<void>(
+      future: populateEvents(),
+      builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.none:
+            return new Text('Press button to start');
+          case ConnectionState.waiting:
+            return new Text('Awaiting results from Meeting API...');
+          default:
+            if (snapshot.hasError)
+              return new Text('Error: ${snapshot.error}');
+            else
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          "Next meetings:",
+                          style: TextStyle(
+                              color: const Color(0xff3f515e),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 42),
+                        ),
+                      ]),
+                  Container(
+                      color: const Color(0xff3f515e),
+                      width: MediaQuery.of(context).size.width * 0.7,
+                      height: MediaQuery.of(context).size.height * 0.15,
+                      child: CarouselSlider(
+                          enableInfiniteScroll: false,
+                          items: _events.map((event) {
+                            return generateEventView(event);
+                          }).toList())),
+                ],
+              );
+        }
+      },
     );
   }
 }
